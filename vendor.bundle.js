b382c12be48019a989dfec176868672201946df0
NatasaPeic <peic.natasa@gmail.com>
2016-08-21 14:36:05 -0400

    Changes

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(44);
+	module.exports = __webpack_require__(39);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 44:
+/***/ 39:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
